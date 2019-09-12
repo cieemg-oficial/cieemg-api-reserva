6ead1122ed5b27febd9be1d05900dcab8b2b86a1
Pedro Fortunato <fun.pedro.fortunato@cieemg.bhz>
2019-09-12 14:16:32 -0300

    at

--- src/routes/routerCurso.js
+++ src/routes/routerCurso.js
@@ -3,8 +3,11 @@ const routes =  express.Router();
 const curso = require('../controllers/curso');
 
 routes.get('/cursos', (req, res) => {
-    curso.buscarCursos().then(data => {
-        res.status(200).send(data);
+    curso.buscarCursos().then(cursos => {
+        res.status(200).json({
+            error: false,
+            data: cursos
+        });
     }).catch(err => {
         res.status(503).send(err);
     })
